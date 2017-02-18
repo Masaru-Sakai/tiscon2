34bca33f1e2393a9a768d06fa187886393e9af58
Masaru-Sakai <jbmkb058@yahoo.co.jp>
2017-02-19 00:15:12 +0900

    レンダリングするとき要求されてるのはForm？ まずCampaignFormを作った

--- src/main/java/net/unit8/sigcolle/form/CampaignForm.java
+++ src/main/java/net/unit8/sigcolle/form/CampaignForm.java
@@ -2,6 +2,7 @@ package net.unit8.sigcolle.form;
 
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -13,6 +14,18 @@ public class CampaignForm extends FormBase {
     @DecimalMin("1")
     @DecimalMax("9999")
     private String campaignId;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String statement;
+
+    @NotNull
+    private String goal;
+
+    @NotNull
+    private String createUserId;
 
     public Long getCampaignIdLong() {
         return Long.parseLong(campaignId);
